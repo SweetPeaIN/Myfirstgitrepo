79a0d9b7bca8955b9473e9f0149fa51704710f2c
Aryan Trivedi <cr7aryan7@gmail.com>
2023-05-10 13:56:14 +0530

    added a power function

--- power.cpp
+++ power.cpp
@@ -1,2 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
+double power(int exponent, double base){
+    double result = 1;
+    for (int i = 0; i < exponent; i++){
+        result = result * base;
+    }
+    cout<<"The exponention "<<base<<" to power "<<exponent<<" is "<<result<<endl;
+}
+
+int main(){
+    cout<<"Enter the exponent: ";
+    int exponent;
+    cin>>exponent;
+    cout<<"Enter the base: ";
+    double base;
+    cin>>base;
+    power(exponent, base);
+    return 0;
+}
